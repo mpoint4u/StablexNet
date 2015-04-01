789ff6439810d3ca1c721f8293f0c99904a92146
Marcus Pointner <marcus.pointner@gmail.com>
2015-04-01 17:01:15 +0300

    added new FD OpenFL haxe project for the Client

---
+++ src/Main.hx
@@ -0,0 +1,23 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+import ru.stablex.net.SxServer;
+
+/**
+ * ...
+ * @author pm
+ */
+
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+}
